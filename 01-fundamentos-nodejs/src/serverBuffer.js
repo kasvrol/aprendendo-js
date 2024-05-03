88030be3ca53dc6feaf2ca7fcf96fe354075a2b2
kasvrol <dovalekarolina@gmail.com>
2024-05-03 11:33:19 -0300

    feat: exemplo de server hard code utilizando api buffer

---
+++ 01-fundamentos-nodejs/src/serverBuffer.js
@@ -0,0 +1,33 @@
+//Exemplo de server usando api buffer
+import http from "http";
+import { bufferToJson } from "./middleware/bufferToJson.js";
+import { Database } from "./database.js";
+import { randomUUID } from "crypto";
+
+const database = new Database();
+
+const server = http.createServer(async (req, res) => {
+  const { method, url } = req;
+
+  await bufferToJson(req, res);
+
+  if (method === "GET" && url === "/users") {
+    return res.end(JSON.stringify(database.select("users")));
+  }
+  if (method === "POST" && url === "/users") {
+    const { name, email } = req.body;
+
+    const user = {
+      id: randomUUID(),
+      name,
+      email,
+    };
+
+    database.insert("users", user);
+    return res.writeHead(201).end();
+  }
+
+  return res.writeHead(404).end();
+});
+
+server.listen(3333);
